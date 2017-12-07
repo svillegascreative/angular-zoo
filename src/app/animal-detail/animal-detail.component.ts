096fdb8e9b9fa728f9f2146bd278d6d3a695313b
sylvia villegas <sylvia@svillegas.com>
2017-12-07 12:03:43 -0500

    Add the Input() Animal property

--- src/app/animal-detail/animal-detail.component.ts
+++ src/app/animal-detail/animal-detail.component.ts
@@ -1,12 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Animal } from '../animal';
 
 @Component({
   selector: 'app-animal-detail',
   templateUrl: './animal-detail.component.html',
   styleUrls: ['./animal-detail.component.scss']
 })
+
 export class AnimalDetailComponent implements OnInit {
 
+  @Input() animal: Animal;
+  
   constructor() { }
 
   ngOnInit() {
