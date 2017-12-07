16c747ed7584cb97fd947746945b2b97b58d0ea9
sylvia villegas <sylvia@svillegas.com>
2017-12-07 13:09:08 -0500

    Populate Animal service

--- src/app/animal.service.ts
+++ src/app/animal.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@angular/core';
+import { Animal } from './animal';
+import { ANIMAL } from './mock-animals';
 
 @Injectable()
 export class AnimalService {
 
   constructor() { }
+
+  getAnimals(); Animal[] {
+    return ANIMALS;
+  }
 
 }
