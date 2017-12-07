8e42009a573cff3edde99c0c159ec648f4efc03f
sylvia villegas <sylvia@svillegas.com>
2017-12-07 15:30:30 -0500

    Add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { AnimalsComponent } from './animals/animals.component';
+
+const routes: Routes = [
+  {
+    path: 'animals',
+    component: AnimalsComponent
+  }
+]
+
 @NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
   exports: [
     RouterModule
   ]
 })
+
 export class AppRoutingModule { }
