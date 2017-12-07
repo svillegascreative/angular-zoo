c867222e88e36a9e385c168db14018a7145f1a29
sylvia villegas <sylvia@svillegas.com>
2017-12-07 14:26:44 -0500

    Inject Message sevice into Animal service

--- src/app/animal.service.ts
+++ src/app/animal.service.ts
@@ -4,11 +4,12 @@ import { of } from 'rxjs/observable/of';
 
 import { Animal } from './animal';
 import { ANIMALS } from './mock-animals';
+import { MessageService } from './message.service';
 
 @Injectable()
 export class AnimalService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   getAnimals(): Observable<Animal[]> {
     return of(ANIMALS);
