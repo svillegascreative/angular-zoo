3ca78489ae1fbcbe06fbcfa51008863537c63bc1
sylvia villegas <sylvia@svillegas.com>
2017-12-08 14:25:24 -0500

    Move mock data to InMemory DB

---
+++ src/app/in-memory-data.service.ts
@@ -0,0 +1,73 @@
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+
+export class InMemoryDataService implements InMemoryDbService {
+  createDb() {
+    const animals = [
+      {
+        id: 1,
+        name: 'Rolly',
+        name_common: 'nine-banded armadillo',
+        name_sci: 'Dasypus novemcinctus',
+        descriptor: 'magnificent',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/b/bc/Nine-banded_Armadillo_%28280204298%29.jpg'
+      },
+      {
+        id: 2,
+        name: 'Noodle',
+        name_common: 'blue wildebeest',
+        name_sci: 'Connochaetes gnou',
+        descriptor: 'show-stopping',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Black_Wildebeest_%28Connochaetes_gnou%29_%2831746882054%29.jpg/1024px-Black_Wildebeest_%28Connochaetes_gnou%29_%2831746882054%29.jpg'
+      },
+      {
+        id: 3,
+        name: 'Pointer',
+        name_common: 'blue-footed booby',
+        name_sci: 'Sula nebouxii',
+        descriptor: 'splendiloquent',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/Blue-footed_Booby_%28Sula_nebouxii%29_-one_leg_raised.jpg/800px-Blue-footed_Booby_%28Sula_nebouxii%29_-one_leg_raised.jpg'
+      },
+      {
+        id: 4,
+        name: 'Fluffy',
+        name_common: 'red rock crab',
+        name_sci: 'Graspus graspus',
+        descriptor: 'sweet and sensitive',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/Zayapa_%28Grapsus_grapsus%29%2C_Las_Bachas%2C_isla_Santa_Cruz%2C_islas_Gal%C3%A1pagos%2C_Ecuador%2C_2015-07-23%2C_DD_30.jpg/1024px-Zayapa_%28Grapsus_grapsus%29%2C_Las_Bachas%2C_isla_Santa_Cruz%2C_islas_Gal%C3%A1pagos%2C_Ecuador%2C_2015-07-23%2C_DD_30.jpg'
+      },
+      {
+        id: 5,
+        name: 'Agador Spartacus',
+        name_common: 'kaiser\'s spotted newt',
+        name_sci: 'Neurergus kaiseri',
+        descriptor: 'bombastic',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/d/dd/Neurergus_Kaiseri.jpg'
+      },
+      {
+        id: 7,
+        name: 'Dexter',
+        name_common: 'bicolor parrotfish',
+        name_sci: 'Cetoscarus bicolor',
+        descriptor: 'glamorous',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/d/dd/Bicolor_parrotfish.JPG'
+      },
+      {
+        id: 8,
+        name: 'Zip',
+        name_common: 'cow-nosed ray',
+        name_sci: 'Rhinoptera bonasus Brest',
+        descriptor: 'mellifluous',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/Rhinoptera_bonasus_Brest.jpg/800px-Rhinoptera_bonasus_Brest.jpg'
+      },
+      {
+        id: 9,
+        name: 'Spudley',
+        name_common: 'caiman',
+        name_sci: 'Caiman yacare',
+        descriptor: 'rambunctious',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Caiman_yacare_BM.jpg/800px-Caiman_yacare_BM.jpg'
+      }
+    ];
+    return {animals};
+  }
+}
