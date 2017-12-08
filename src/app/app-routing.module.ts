a33ad79dda8bc2dcd191b9fd1890df204b5aaae3
sylvia villegas <sylvia@svillegas.com>
2017-12-08 10:22:37 -0500

    Add route to animal details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,21 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AnimalsComponent } from './animals/animals.component';
+import { AnimalDetailComponent } from './animal-detail/animal-detail.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    redirectTo: '/dashboard',
-    pathMatch: 'full'
-  },
-  {
-    path: 'dashboard',
-    component: DashboardComponent
-  },
-  {
-    path: 'animals',
-    component: AnimalsComponent
-  }
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'animals', component: AnimalsComponent },
+  { path: 'animal/:id', component: AnimalDetailComponent }
 ]
 
 @NgModule({
