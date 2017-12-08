003ff83a39aafe7fbaa00112717c90e94521511d
sylvia villegas <sylvia@svillegas.com>
2017-12-08 14:32:10 -0500

    Update animal service to use http client

--- src/app/animal.service.ts
+++ src/app/animal.service.ts
@@ -1,24 +1,34 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
 
 import { Animal } from './animal';
-import { ANIMALS } from './mock-animals';
 import { MessageService } from './message.service';
 
 @Injectable()
 export class AnimalService {
 
-  constructor(private messageService: MessageService) { }
+  private animalsUrl = 'api/animals';
+
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+  ) { }
 
   getAnimals(): Observable<Animal[]> {
     this.messageService.add('So many animals to see!');
-    return of(ANIMALS);
+    return this.http.get<Animal[]>(this.animalsUrl);
   }
 
   getAnimal(id: number): Observable<Animal> {
     this.messageService.add(`Visited animal #${id}!`);
     return of(ANIMALS.find(animal => animal.id === id));
+  }
+
+  private log(message: string) {
+    this.messageService.add('AnimalService: ' + message);
   }
 
 }
