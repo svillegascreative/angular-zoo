940e7d6080eb74ec02f71ad782474888b7c55807
sylvia villegas <sylvia@svillegas.com>
2017-12-11 09:59:23 -0500

    Update animals tpl: form to add animal

--- src/app/animals/animals.component.ts
+++ src/app/animals/animals.component.ts
@@ -23,4 +23,19 @@ export class AnimalsComponent implements OnInit {
         .subscribe(animals => this.animals = animals)
   }
 
+  add(commonName: string, sciName: string, indivName?: string, image?: string, descriptor?: string): void {
+    const newAnimal = {
+      name_common: commonName.trim(),
+      name_sci: sciName.trim(),
+      name: indivName.trim(),
+      image: image.trim(),
+      descriptor: descriptor.trim()
+    }
+    if (!newAnimal.name_common && !newAnimal.name_sci) {
+      return
+    }
+    this.animalService.addAnimal(newAnimal as Animal)
+      .subscribe(animal => this.animals.push(newAnimal as Animal));
+  }
+
 }
