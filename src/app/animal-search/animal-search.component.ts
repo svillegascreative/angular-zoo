f1061fba82d6418595238fbd224c67bb3422f8f3
sylvia villegas <sylvia@svillegas.com>
2017-12-12 12:52:41 -0500

    Populate animal search cmpt

--- src/app/animal-search/animal-search.component.ts
+++ src/app/animal-search/animal-search.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
+import { of } from 'rxjs/observable/of';
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
+
+import { Animal } from '../animal';
+import { AnimalService } from '../animal.service';
+
 @Component({
   selector: 'app-animal-search',
   templateUrl: './animal-search.component.html',
   styleUrls: ['./animal-search.component.scss']
 })
+
 export class AnimalSearchComponent implements OnInit {
 
-  constructor() { }
+  animals$: Observable<Animal[]>;
+  private searchTerms = new Subject<string>();
 
-  ngOnInit() {
+  constructor(private animalService: AnimalService) { }
+
+  search(term: string): void {
+    this.searchTerms.next(term);
+  }
+
+  ngOnInit(): void {
+    this.animals$ = this.searchTerms.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap( (term: string) => this.animalService.searchAnimals(term)),
+    );
   }
 
 }
